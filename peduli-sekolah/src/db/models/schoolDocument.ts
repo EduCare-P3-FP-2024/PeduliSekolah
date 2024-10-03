ba5352c6f0adb5050eb11c8a80e552182aec7efe
Zhafir <zahfir1000@hotmail.com>
2024-10-03 14:41:28 +0700

    feat: models payee schooldocument transaction vote and fixed in models post

---
+++ peduli-sekolah/src/db/models/schoolDocument.ts
@@ -0,0 +1,50 @@
+import { SchoolDocument, SchoolDocumentInput } from "@/utils/types";
+import { getDb } from "./user";
+import { ObjectId } from "mongodb";
+
+const COLLECTION_DOCUMENT = "schoolDocuments";
+
+export const getDocuments = async () => {
+  const db = await getDb();
+
+  const documents = (await db
+    .collection(COLLECTION_DOCUMENT)
+    .find({})
+    .toArray()) as SchoolDocument[];
+
+  return documents;
+};
+
+export const getDocumentById = async (id: string) => {
+  const db = await getDb();
+
+  const document = (await db
+    .collection(COLLECTION_DOCUMENT)
+    .findOne({ _id: new ObjectId(id) })) as SchoolDocument;
+
+  return document;
+};
+
+export const getDocumentByUserId = async (id: string) => {
+  const db = await getDb();
+
+  const document = (await db
+    .collection(COLLECTION_DOCUMENT)
+    .findOne({ userId: new ObjectId(id) })) as SchoolDocument;
+
+  return document;
+};
+
+export const addDocument = async (document: SchoolDocumentInput) => {
+  const db = await getDb();
+  const modifiedDocument = {
+    ...document,
+    userId: new ObjectId(document.userId),
+  };
+
+  const result = await db
+    .collection(COLLECTION_DOCUMENT)
+    .insertOne(modifiedDocument);
+
+  return result;
+};
---
+++ peduli-sekolah/src/db/models/transaction.ts
@@ -0,0 +1,51 @@
+import { Transaction, TransactionInput } from "@/utils/types";
+import { getDb } from "./user";
+import { ObjectId } from "mongodb";
+
+const COLLECTION_TRANSACTION = "transactions";
+
+export const getTransactions = async () => {
+  const db = await getDb();
+
+  const transactions = (await db
+    .collection(COLLECTION_TRANSACTION)
+    .find({})
+    .toArray()) as Transaction[];
+
+  return transactions;
+};
+
+export const getTransactionById = async (id: string) => {
+  const db = await getDb();
+
+  const transaction = (await db
+    .collection(COLLECTION_TRANSACTION)
+    .findOne({ _id: new ObjectId(id) })) as Transaction;
+
+  return transaction;
+};
+
+export const getTransactionByUserId = async (userId: string) => {
+  const db = await getDb();
+
+  const transaction = (await db
+    .collection(COLLECTION_TRANSACTION)
+    .findOne({ userId: new ObjectId(userId) })) as Transaction;
+
+  return transaction;
+};
+
+export const AddTransaction = async (transaction: TransactionInput) => {
+  const db = await getDb();
+
+  const modifiedTransaction = {
+    ...transaction,
+    userId: new ObjectId(transaction.userId),
+  };
+
+  const result = await db
+    .collection(COLLECTION_TRANSACTION)
+    .insertOne(modifiedTransaction);
+
+  return result;
+};
