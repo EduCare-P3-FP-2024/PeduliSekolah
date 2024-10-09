bcb0e1ed43b5b14764263ee18cf25ace96219d5d
ffznrr <ffznrr@gmail.com>
2024-10-09 16:28:49 +0700

    fixing landing page

--- peduli-sekolah/src/components/CTALandingPage.tsx
+++ peduli-sekolah/src/components/CTALandingPage.tsx
@@ -1,10 +1,11 @@
 "use client";
 
-import React, { ReactNode } from "react";
+import React, { ReactNode, useState } from "react";
 import { motion, useAnimation } from "framer-motion";
 import { useInView } from "react-intersection-observer";
 import { useEffect } from "react";
 import Link from "next/link";
+import { getAuthCookies } from "@/app/admin/SchoolList/action";
 
 interface FadeInWhenVisibleProps {
   children: ReactNode;
@@ -37,6 +38,18 @@ const FadeInWhenVisible = ({ children }: FadeInWhenVisibleProps) => {
 };
 
 export default function CTALandingPage() {
+  const [token, setToken] = useState<object>();
+
+  useEffect(() => {
+    const fethingToken = async () => {
+      {
+        const fetchToken = await getAuthCookies();
+        setToken(fetchToken);
+      }
+    };
+
+    fethingToken();
+  }, []);
   return (
     <div className="container mx-auto px-4 text-center">
       <FadeInWhenVisible>
@@ -47,12 +60,21 @@ export default function CTALandingPage() {
           Your support can transform the educational experience for thousands of
           students.
         </p>
-        <Link
-          href="/register"
-          className="bg-[#2C3E50] text-[#ECF0F1] px-8 py-3 rounded-md hover:bg-[#27AE60] transition-colors duration-300 inline-block"
-        >
-          Sign Up Now
-        </Link>
+        {token ? (
+          <Link
+            href="/post"
+            className="bg-[#2C3E50] text-[#ECF0F1] px-8 py-3 rounded-md hover:bg-[#27AE60] transition-colors duration-300 inline-block"
+          >
+            Donate Now
+          </Link>
+        ) : (
+          <Link
+            href="/register"
+            className="bg-[#2C3E50] text-[#ECF0F1] px-8 py-3 rounded-md hover:bg-[#27AE60] transition-colors duration-300 inline-block"
+          >
+            Sign Up Now
+          </Link>
+        )}
       </FadeInWhenVisible>
     </div>
   );
